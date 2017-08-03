2e0fc2d97abe79b2834554fc17e972911848bf8a
Brett Mayson <brett@bmandesigns.com>
2017-08-02 20:48:16 -0600

    htaccess and login page

--- dash/index.php
+++ dash/index.php
@@ -5,7 +5,12 @@
   $a = GetFromURL('a','home');
 
   if (file_exists("pages/$a.php")) {
-
+    include("pages/$a.php");
+    if ($PAGE_TYPE == "custom") {
+      OutputPage();
+    } else {
+      input("template.php");
+    }
   } else {
     include("pages/404.php");
     include("template.php");
---
+++ dash/pages/login.php
@@ -0,0 +1,61 @@
+<?php
+  $PAGE_TYPE = "custom";
+
+  function OutputPage() {?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Sign in | CCDash</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="css/admin.min.css">
+
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    </head>
+    <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="#"><b>CC</b>Dash</a>
+      </div>
+      <div class="login-box-body">
+        <p class="login-box-msg">Sign in</p>
+
+        <form action="#" method="post">
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="Username">
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div>
+          </div>
+        </form>
+        <br/>
+        <a href="#">Reset Password</a><br>
+
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+    </html>
+  <?php }
+?>
