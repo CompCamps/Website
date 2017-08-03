111aaa4d037448cb317632cedabaf4826e8b19e1
Brett Mayson <brett@bmandesigns.com>
2017-08-02 20:28:25 -0600

    Initial Upload

--- dash/index.php
+++ dash/index.php
@@ -2,13 +2,13 @@
 
   require_once("../config.php");
 
-  $a = GetFromURL('a','campers');
+  $a = GetFromURL('a','home');
 
   if (file_exists("pages/$a.php")) {
 
   } else {
     include("pages/404.php");
-    OutputPage();
+    include("template.php");
     exit;
   }
 ?>
