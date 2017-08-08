d79ac95a65f0d823ea854ae4f0bdd90720a2670e
rye761 <ryebread761@gmail.com>
2017-08-08 00:10:14 -0600

    faq styling and first section content

--- includes/header.php
+++ includes/header.php
@@ -3,9 +3,9 @@
       <img id="header-img" src="img/logo.png">
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="./index.php">Home</a></li>
           <li><a href="#">Campers</a></li>
-          <li><a href="#">FAQ</a></li>
+          <li><a href="./faq.php">FAQ</a></li>
           <li><a href="#">Mentors</a></li>
           <li><a href="#">Register</a></li>
         </ul>
