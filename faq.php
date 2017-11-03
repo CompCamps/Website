0c9f510231c28f73cda87d060541e13f490d24c9
rye761 <ryebread761@gmail.com>
2017-11-02 21:28:14 -0600

    Second FAQ section

--- faq.php
+++ faq.php
@@ -18,5 +18,67 @@ include('includes/header.php');
   <p class="indent">Each day between 8:30 a.m. and 8:30 p.m. you’ll work on your projects as an individual and in small teams. Naturally there are biobreaks and activity breaks. Evenings include group technical, social, and recreational activities. Your camp fee covers all activites. Late morning or early afternoon activities (depending on the weather) include swimming and team games. Monday evenings offer bowling, Tuesday and Thursday evenings are for individual and team project work as well as for special guest presentations to small groups. Wednesday evenings are for competitive computer games. Friday afternoon is highlighted by a closing ceremony where campers get to show their creations to family and friends. In addition, the camps provide lasting benefits by enhancing your technical, creative, and communication skill sets. You return home better able to assist the advanced technological challenges and opportunities faced by your family, schools, and community groups.</p>
 
 </section>
+<section class="faq-section">
+  <h2 class="text-center">What’s the camp schedule?</h2>
+  <h3>How does each camp begin?</h3>
+  <p class="indent">Camp opens each week on Monday at 8:30 a.m. at the University of Regina. After a brief introduction to staff, camp rules, and procedures, everybody dives right into action! During the first Monday session, mentors evaluate each camper’s abilities and goals to ensure learning at an appropriate level.</p>
+  <h3>What’s the typical daily schedule for Tuesdays through Thursdays?</h3>
+  <table id="schedule">
+    <tr>
+      <th>Time</th>
+      <th>Activity</th>
+    </tr>
+    <tr>
+      <td>8:30 am</td>
+      <td>Daily Briefing</td>
+    <tr/>
+    <tr>
+      <td>8:45 am</td>
+      <td>Session One</td>
+    <tr/>
+    <tr>
+      <td>10:00 am</td>
+      <td>First Biobreak</td>
+    <tr/>
+    <tr>
+      <td>10:15 am</td>
+      <td>Session Two</td>
+    <tr/>
+    <tr>
+      <td>12:00 pm</td>
+      <td>Lunch Break</td>
+    <tr/>
+    <tr>
+      <td>12:30 pm</td>
+      <td>Free Time</td>
+    <tr/>
+    <tr>
+      <td>1:15 pm</td>
+      <td>Session 3</td>
+    <tr/>
+    <tr>
+      <td>3:00 pm</td>
+      <td>Second Biobreak</td>
+    <tr/>
+    <tr>
+      <td>3:15 pm</td>
+      <td>Session Four</td>
+    <tr/>
+    <tr>
+      <td>5:00 pm</td>
+      <td>Supper Break</td>
+    <tr/>
+    <tr>
+      <td>7:00 pm</td>
+      <td>Evening Activities</td>
+    <tr/>
+    <tr>
+      <td>8:45 pm</td>
+      <td>Home Time</td>
+    <tr/>
+  </table>
+  <h3>What are the week's features?</h3>
+  <p class="indent">The daily program begins when the computer lab opens at 8:30 a.m. Monday through Friday. Each Monday features a pizza party and bowling. Campers get picked up from bowling around 7:45 p.m. Tuesday through Thursday the formal program ends at 5:00 p.m. Most campers choose to participate in the optional evening activities held from 5:00 p.m. through to 8:45 p.m. The optional activities include opportunities for project work, multi-player computer games, and special interest topics. On Fridays campers spend the morning hours finishing their projects and at 3:00 p.m. the camp closes with campers presentations and awards, where each camper’s achievements are highlighted for families, teachers, and other invited guests.</p>
+</section>
 
 <?php include('includes/footer.php'); ?>
