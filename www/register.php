8f9bce3f410d0665c64c0b7296313831e1696639
trollmad3 <7843719+trollmad3@users.noreply.github.com>
2018-06-24 16:49:44 -0600

    Update register.php
    I noticed the first week is closed and it will help out people. Let's say they enter all the registration and find out the first week is closed at the end.

--- www/register.php
+++ www/register.php
@@ -9,6 +9,8 @@ include('includes/header.php');
 <section class="register-section">
   <h2>Registration is Open</h2>
   <h3>$350</h3>
+  <h3>First week: <b>CLOSED</b></h3>
+  <h3>Secound week: OPEN</h3>
   <a class="button" href="dash/register">Register</a>
 </section>
 
