2916aa74e4c94cea83f85fc3b6f837cfc6de7f8d
Kaitlin <kzastrow01@gmail.com>
2022-08-04 17:17:05 -0600

    Camper website checker.
    Created a website checker to see if a camper's website is live on github.

---
+++ tools/website-checker.js
@@ -0,0 +1,18 @@
+import urlExist from 'url-exist';
+import { createReadStream } from "fs";
+import { parse } from "csv-parse";
+
+
+createReadStream("../src/assets/camper-info.csv")
+    .pipe(parse({ delimiter: ",", from_line: 1 }))
+    .on("data", function (row) {
+        async function execute() {
+            let [name, github, repo] = row;
+            let url = `https://${github}.github.io/${repo}`;
+            const exists = await urlExist(url);
+            console.log(name,exists);
+        }
+        (async() => {
+            await execute();
+        })();
+    })
